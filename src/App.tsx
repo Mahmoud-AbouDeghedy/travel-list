cec00e4183619fae839e85250c8ff312ebc19389
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-07-05 01:12:55 +0300

    setting most of the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+type itemT = {
+	id: number;
+	description: string;
+	quantity: number;
+	packed: boolean;
+};
+
 export default function App() {
 	return (
 		<div className="app">
@@ -14,15 +28,70 @@ function Logo() {
 }
 
 function Form() {
+	const [text, setText] = useState("");
+	const [quantity, setQuantity] = useState(1);
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		if (!text) return;
+
+		const newItem: itemT = {
+			id: Date.now(),
+			description: text,
+			quantity,
+			packed: false,
+		};
+
+		setText("");
+		setQuantity(1);
+	}
+
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-		</div>
+			<select
+				name=""
+				id=""
+				value={quantity}
+				onChange={(e) => setQuantity(+e.target.value)}
+			>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((i, idx) => (
+					<Item item={i} key={idx} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item }: { item: itemT }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats() {
